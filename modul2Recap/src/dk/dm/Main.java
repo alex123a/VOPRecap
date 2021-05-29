b2f34708470098c938d6519a920e86795abe00a3
Alex Valentiner (alex123a) <alex.valentiner@hotmail.com>
2021-05-29 15:35:37 +0200

    s

--- modul2Recap/src/dk/dm/Main.java
+++ modul2Recap/src/dk/dm/Main.java
@@ -1,8 +1,18 @@
 package dk.dm;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
 public class Main {
 
+    static PrintWriter p;
+
     public static void main(String[] args) {
-	// write your code here
+        try {
+            p = new PrintWriter("test.csv");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
